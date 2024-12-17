3afa1eef0bafd24706fa7d6bf8562770a659f91a
ajax301096 <147789857+ajax301096@users.noreply.github.com>
2024-12-17 14:59:30 +0900

    #

--- KF31_WebApp/obj/Debug/net8.0/KF31_WebApp.AssemblyInfo.cs
+++ KF31_WebApp/obj/Debug/net8.0/KF31_WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KF31_WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b4bfc53ebf6035b545c94bbc357dca140e6bbac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5877d25be92e61faa1c9752e7a653ea7125e2b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("KF31_WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KF31_WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
